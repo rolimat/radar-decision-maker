486d13f31e967fd2a824b23a42360ef789bc40ab
Alberto Mateos <alberto.mateos@clarity.ai>
2021-04-12 17:43:50 +0200

    Added i18n support

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,29 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    i18nTEST: {{ t('teest') }}
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import { useI18n } from 'vue-i18n' // @ is an alias to /src
 
 export default defineComponent({
   name: 'Home',
   components: {
     HelloWorld
+  },
+  setup () {
+    const { t } = useI18n({
+      inheritLocale: true,
+      useScope: 'local'
+    })
+
+    // Something todo ..
+
+    return { t }
   }
 })
 </script>
